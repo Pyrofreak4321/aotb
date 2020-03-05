454d4fac42f6538ba9b6fd2587634d298b1cdc82
AnthonyMork <pyrofreak4321@gmail.com>
2020-03-05 15:33:50 -0500

    Algorithm update added range and direction check to try and kill tracks early. renamed worker.

--- generator.js
+++ generator.js
@@ -8,7 +8,7 @@ function left(dir){
   if(dir[0]==-1 && dir[1]==0){
     a[0] = 0;
     a[1] = -1;
-  } else if(dir[0]==0 && dir[1]== -1){
+  } else if(dir[0]==0 && dir[1]==-1){
     a[0] = 1;
     a[1] = 0;
   } else if(dir[0]==1 && dir[1]==0){
@@ -39,8 +39,44 @@ function right(dir){
   return a;
 }
 
+function testDir(track){
+  var dir = track.pieces[track.pieces.length-1].dir;
+  var result = 1;
+  if(dir[0]==-1 && dir[1]==0){
+    if(track.pool[1] < 1){
+      result = 0;
+    }
+  } else if(dir[0]==1 && dir[1]==0){
+    if(track.pool[1] < 1){
+      result = 0;
+    }
+  } else if(dir[0]==0 && dir[1]==1){
+    if(track.pool[1] < 2){
+      result = 0;
+    }
+  }
+  return result;
+}
+
+function testRange(track){
+  var dist = Math.abs(track.pieces[track.pieces.length-1].pos[0])+Math.abs(track.pieces[track.pieces.length-1].pos[1])-1;
+  var range = 0;
+  for(i = 0; i < track.pool.length; i++){
+    if(i == 1 || i==2){
+      range += (track.pool[i]/2);
+    } else {
+      range += track.pool[i];
+    }
+  }
+  if(range < dist){
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
 //check if slot is open
-function posOpen(track){
+function posOpen(track, type){
   var lastpiece = track.pieces[track.pieces.length-1];
   var hit = 1;
   var pos = [lastpiece.pos[0]+lastpiece.dir[0],lastpiece.pos[1]+lastpiece.dir[1]];
@@ -48,11 +84,32 @@ function posOpen(track){
     if(track.pieces[i].pos[0] == pos[0] && track.pieces[i].pos[1] == pos[1]){
       hit = 0;
       //check if is start piece
-      if((track.pieces[i].type == -1 && track.pieces[i].dir[0] == lastpiece.dir[0] && track.pieces[i].dir[1] == lastpiece.dir[1])){
+      if(type != 4 && (track.pieces[i].type == -1 && track.pieces[i].dir[0] == lastpiece.dir[0] && track.pieces[i].dir[1] == lastpiece.dir[1])){
         hit = -1;
+      }
+    } else if(track.pieces[i].type == 4 && ((track.pieces[i].pos[0]-track.pieces[i].dir[0])== pos[0] && (track.pieces[i].pos[1]-track.pieces[i].dir[1]) == pos[1])){
+      hit = 0;
+    }
+  }
+
+  if(type == 4 && hit == 1){
+    pos[0] += lastpiece.dir[0];
+    pos[1] += lastpiece.dir[1];
+    for(var ii = 0; ii < track.pieces.length && hit==1; ii++){
+      if(track.pieces[ii].pos[0] == pos[0] && track.pieces[ii].pos[1] == pos[1]){
+        hit = 0;
       }
     }
   }
+
+  if(hit == 1){
+    hit = testRange(track);
+  }
+
+  if(hit == 1){
+    hit = testDir(track);
+  }
+
   return hit;
 }
 
@@ -74,7 +131,6 @@ function print(track){
   return s;
 }
 
-//gets char code for piece
 function stringify(p){
   var s=' ';
   switch (p.type) {
@@ -90,16 +146,48 @@ function stringify(p){
     case 2:
       s = 'c';
       break;
+    case 4:
+      s = 'j';
+      break;
+    case 6:
+      s = 'b';
+      break;
   }
   return s;
 }
 
+function filter(item){
+  var good = true;
+  var path = item.path+item.path;
+  for(var y = 0; y < goodTracks.length && good; y++){
+    if(goodTracks[y].path.length == item.path.length && path.includes(goodTracks[y].path) ){
+      good = false;
+    }
+  }
+  if(good){
+    goodTracks.push(item);
+    //postMessage({type: 0, tracks: item});
+  }
+}
+
+function storeTrack(track){
+  var item = {
+    pieces: JSON.parse(JSON.stringify(track.pieces)),
+    path:'',
+  };
+  for(var i = 0; i < item.pieces.length; i++){
+    item.path += stringify(item.pieces[i]);
+  }
+  filter(item);
+  // postMessage({type: 0, track: item});
+}
+
 //adds peoce to track
 function addPiece(track, type){
-  var result = posOpen(track);
+  var result = posOpen(track,type);
   if(result==1){
     var lastpiece = track.pieces[track.pieces.length-1];
-    if(type == 0){
+    if(type == 0 || type == 6){
       track.pieces.push({
         type:type,
         pos:[lastpiece.pos[0]+lastpiece.dir[0],lastpiece.pos[1]+lastpiece.dir[1]],
@@ -125,27 +213,20 @@ function addPiece(track, type){
       track.pool[type]--;
       track.pool[type-1]--;
     }
+    else if(type == 4){
+      track.pieces.push({
+        type:type,
+        pos:[lastpiece.pos[0]+(lastpiece.dir[0]*2),lastpiece.pos[1]+(lastpiece.dir[1]*2)],
+        dir: [lastpiece.dir[0],lastpiece.dir[1]],
+      });
+      track.pool[type]--;
+    }
 
+    if(posOpen(track) == -1){
+      storeTrack(track);
+      return -1;
+    }
     return nextPiece(track,-1);
-  }
-  else if(result==-1){
-    var item = {
-      pieces: track.pieces,
-      path: track.pieces.map(stringify).join('')
-    };
-
-    var good = true;
-    var path = item.path+item.path;
-    for(var x = 0; x < goodTracks.length; x++){
-      if(path.includes(goodTracks[x].path) && goodTracks[x].path.length == item.path.length){
-        good = false;
-      }
-    }
-
-    if (good){
-      goodTracks.push(JSON.parse(JSON.stringify(item))); //super simple clone
-    }
-    return -1;
   }
   else{
     return -1;
@@ -181,6 +262,7 @@ function doGen(track,type){
       result = pop(track);
     }while(result < 0 && track.pieces.length > 0);
     if(track.pieces.length > 0){
+      //console.log(result);
       return result;
     }
   }
@@ -194,7 +276,6 @@ onmessage = function(e) {
   goodTracks = [];
 
   var newTrack = {
-    //      S L R r j i s
     pool : e.data,
     pieces:[{
       type:-1,
@@ -209,5 +290,5 @@ onmessage = function(e) {
   }while(doGenRes >= 0);
 
   //send good tracks back
-  postMessage(goodTracks);
+  postMessage({type: 1, tracks: goodTracks});
 };
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-Use <i>Start-Process "chrome.exe" "--allow-file-access-from-files"</i> from Windows powershell with all browsers closed or run from server (node/php/apache)
+Use Start-Process "chrome.exe" "--allow-file-access-from-files" from Windows powershell with all browsers closed
+or run from server (node/php/apache)
